0f96b54f97c6fa47cda8ec34e8d1f48855d34603
Nguyễn Hoàng Thiên Ân <57244622+s3825455@users.noreply.github.com>
2020-05-02 23:03:25 +0700

    a3 update 4

--- a3/script.js
+++ a3/script.js
@@ -2,3 +2,32 @@
 $(document).on('click', 'ul li', function() {
     $(this).addClass('active').siblings().removeClass('active')
 })
+
+function reveal() {
+    document.getElementById("hiddensyn4").style.display = 'none';
+    document.getElementById("hiddensyn3").style.display = 'none';
+    document.getElementById("hiddensyn2").style.display = 'none';
+    document.getElementById("hiddensyn").style.display = 'block';
+}
+
+
+function reveal2() {
+    document.getElementById("hiddensyn4").style.display = 'none';
+    document.getElementById("hiddensyn3").style.display = 'none';
+    document.getElementById("hiddensyn").style.display = 'none';
+    document.getElementById("hiddensyn2").style.display = 'block';
+}
+
+function reveal3() {
+    document.getElementById("hiddensyn4").style.display = 'none';
+    document.getElementById("hiddensyn").style.display = 'none';
+    document.getElementById("hiddensyn2").style.display = 'none';
+    document.getElementById("hiddensyn3").style.display = 'block';  
+}
+
+function reveal4() {
+    document.getElementById("hiddensyn3").style.display = 'none';
+    document.getElementById("hiddensyn").style.display = 'none';
+    document.getElementById("hiddensyn2").style.display = 'none';
+    document.getElementById("hiddensyn4").style.display = 'block';
+}
