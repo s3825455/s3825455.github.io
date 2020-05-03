22be88ac4a9583d21a5da1f5dad045e7e691de38
Nguyễn Hoàng Thiên Ân <57244622+s3825455@users.noreply.github.com>
2020-05-03 21:31:28 +0700

    a3 update 6

--- a3/script.js
+++ a3/script.js
@@ -33,3 +33,74 @@ function reveal4() {
     document.getElementById("hiddensyn4").style.display = 'block';
 }
 
+function AEW9() {
+    document.getElementById("booking_movie").innerHTML = "Avenger Endgame - Wednesday - 9PM"
+}
+
+function AET9() {
+    document.getElementById("booking_movie").innerHTML = "Avenger Endgame - Thursday - 9PM"
+}
+
+function AEF9() {
+    document.getElementById("booking_movie").innerHTML = "Avenger Endgame - Friday - 9PM"
+}
+
+function AESA9() {
+    document.getElementById("booking_movie").innerHTML = "Avenger Endgame - Saturday - 6PM"
+}
+
+function AESU9() {
+    document.getElementById("booking_movie").innerHTML = "Avenger Endgame - Sunday - 6PM"
+}
+
+function DM12() {
+    document.getElementById("booking_movie").innerHTML = "Dumbo - Monday - 12PM"
+}
+
+function DT12() {
+    document.getElementById("booking_movie").innerHTML = "Dumbo - Tuesday - 12PM"
+}
+
+function DW6() {
+    document.getElementById("booking_movie").innerHTML = "Dumbo - Wednesday - 6PM"
+}
+
+function DT6() {
+    document.getElementById("booking_movie").innerHTML = "Dumbo - Tuesday - 6PM"
+}
+
+function DF6() {
+    document.getElementById("booking_movie").innerHTML = "Dumbo - Friday - 6PM"
+}
+
+function DSA12() {
+    document.getElementById("booking_movie").innerHTML = "Dumbo - Saturday - 12PM"
+}
+
+function DSU12() {
+    document.getElementById("booking_movie").innerHTML = "Dumbo - Sunday - 12PM"
+}
+
+function HPM6() {
+    document.getElementById("booking_movie").innerHTML = "The Happy Prince - Monday - 6PM"
+}
+
+function HPT6() {
+    document.getElementById("booking_movie").innerHTML = "The Happy Prince - Tuesday - 6PM"
+}
+
+function HPSA3() {
+    document.getElementById("booking_movie").innerHTML = "The Happy Prince - Saturday - 3PM"
+}
+
+function HPSU3() {
+    document.getElementById("booking_movie").innerHTML = "The Happy Prince - Sunday - 3PM"
+}
+
+function HPSU3() {
+    document.getElementById("booking_movie").innerHTML = "The Happy Prince - Sunday - 3PM"
+}
+
+function TEWW12() {
+    document.getElementById("booking_movie").innerHTML = "Top End Wedding - Sunday - 3PM"
+}
