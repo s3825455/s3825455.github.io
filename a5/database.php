26c01b45259f98cbcba9a22e94f248303d54c762
Nguyễn Hoàng Thiên Ân <57244622+s3825455@users.noreply.github.com>
2020-05-25 20:59:17 +0700

    a5 3

--- a5/database.php
+++ a5/database.php
@@ -1,6 +1,28 @@
 <?php
 $servername = "localhost";
+$username = "root";
+$password = "root";
+$port = 3307;
+
+// Create connection
+$conn = mysqli_connect("$servername:$port", $username, $password);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
 
 
 
+// Create database
+// $sql = "CREATE DATABASE A5DB";
+// if (mysqli_query($conn, $sql)) {
+//   echo "Database created successfully";
+// } else {
+//   echo "Error creating database: " . mysqli_error($conn);
+// }
+
+mysqli_close($conn);
+?>
 ?>
--- week10/database.php
+++ week10/database.php
@@ -3,14 +3,13 @@ $servername = "localhost";
 $username = "root";
 $password = "root";
 $port = 3307;
-$dbname = "myDB";
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
+$conn = mysqli_connect("$servername:$port", $username, $password);
 // Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-
+echo "Connected successfully";
 // // Create database
 // $sql = "CREATE DATABASE myDB";
 // if (mysqli_query($conn, $sql)) {
