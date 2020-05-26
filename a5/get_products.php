e90210fa5cd49e0557833949e6c2e1045e75630d
ManPhamQuang <s3804811@rmit.edu.vn>
2020-05-26 17:29:22 +0700

    added images, index.php

---
+++ a5/get_products.php
@@ -0,0 +1,20 @@
+<?php
+$products = array(
+    'G27504' => array(
+        'name' => 'ULTRABOOST 19',
+        'description' => 'Ultraboost reinvented. These running shoes reboot key performance technologies to give you a confident and energy-filled run. The knit upper has a second-skin fit and is built with motion-weave technology for adaptive stretch and support. Dual-density cushioning delivers medial support and an energised ride. Lace closure\n adidas Primeknit 360 upper\n Synthetic lining; Stretchweb outsole flexes naturally for an energised ride and Continental™ Rubber gives you superior traction\n Dual-density Boost cushioning under arch\n Natural feel like a second skin\n Weight: 309 g (size UK 8.5)\n Midsole drop: 10 mm (heel: 29 mm / forefoot: 19 mm)\n Arch type: Normal; Lightweight 3D heel frame for personalised fit; Stabilising Torsion Spring\n Color',
+        'image' => './media/images/crazychaos-shoes/Crazychaos_Shoes_White_EF1049_010_hover_standard.jpg',
+    ),
+    'EG4316' => array(
+        'name' => 'ULTRABOOST 20',
+        'description' => 'Ultraboost reinvented. These running shoes reboot key performance technologies to give you a confident and energy-filled run. The knit upper has a second-skin fit and is built with motion-weave technology for adaptive stretch and support. Dual-density cushioning delivers medial support and an energised ride. Lace closure\n adidas Primeknit 360 upper\n Synthetic lining; Stretchweb outsole flexes naturally for an energised ride and Continental™ Rubber gives you superior traction\n Dual-density Boost cushioning under arch\n Natural feel like a second skin\n Weight: 309 g (size UK 8.5)\n Midsole drop: 10 mm (heel: 29 mm / forefoot: 19 mm)\n Arch type: Normal; Lightweight 3D heel frame for personalised fit; Stabilising Torsion Spring\n Color'
+    ),
+    'G27501' => array(
+        'name' => 'ULTRABOOST 19',
+        'description' => 'Ultraboost reinvented. These running shoes reboot key performance technologies to give you a confident and energy-filled run. The knit upper has a second-skin fit and is built with motion-weave technology for adaptive stretch and support. Dual-density cushioning delivers medial support and an energised ride. Lace closure\n adidas Primeknit 360 upper\n Synthetic lining; Stretchweb outsole flexes naturally for an energised ride and Continental™ Rubber gives you superior traction\n Dual-density Boost cushioning under arch\n Natural feel like a second skin\n Weight: 309 g (size UK 8.5)\n Midsole drop: 10 mm (heel: 29 mm / forefoot: 19 mm)\n Arch type: Normal; Lightweight 3D heel frame for personalised fit; Stabilising Torsion Spring\n Color'
+    ),
+    'EG2316' => array(
+        'name' => 'ULTRABOOST 20',
+        'description' => 'Ultraboost reinvented. These running shoes reboot key performance technologies to give you a confident and energy-filled run. The knit upper has a second-skin fit and is built with motion-weave technology for adaptive stretch and support. Dual-density cushioning delivers medial support and an energised ride. Lace closure\n adidas Primeknit 360 upper\n Synthetic lining; Stretchweb outsole flexes naturally for an energised ride and Continental™ Rubber gives you superior traction\n Dual-density Boost cushioning under arch\n Natural feel like a second skin\n Weight: 309 g (size UK 8.5)\n Midsole drop: 10 mm (heel: 29 mm / forefoot: 19 mm)\n Arch type: Normal; Lightweight 3D heel frame for personalised fit; Stabilising Torsion Spring\n Color'
+    )
+);
---
+++ a5/tools.php
@@ -0,0 +1,36 @@
+<?php
+  session_start();
+
+// Put your PHP functions and modules here
+
+function test_input($data)
+{
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialChars($data);
+  return $data;
+}
+
+function preShow($arr, $returnAsString = false)
+{
+    $ret  = '<pre>' . print_r($arr, true) . '</pre>';
+    if ($returnAsString)
+        return $ret;
+    else
+        echo $ret;
+}
+
+function printMyCode() {
+
+  $lines = file($_SERVER['SCRIPT_FILENAME']);
+
+  echo "<pre class='mycode'><ol>";
+
+  foreach ($lines as $line)
+
+         echo '<li>'.rtrim(htmlentities($line)).'</li>';
+
+  echo '</ol></pre>';
+
+}
+
