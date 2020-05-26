533862db6ff15dd91d180440f34eefdbac5dd44b
ManPhamQuang <s3804811@rmit.edu.vn>
2020-05-26 22:58:38 +0700

    fetch products

--- a5/database.php
+++ a5/database.php
@@ -65,21 +65,22 @@ $conn = mysqli_connect("$servername:$port", $username, $password, $dbname);
 //   }
 
 
-$sql = "SELECT pid, shoe_name, shoe_description, specs FROM Shoes";
+$sql = "SELECT pid, shoe_name, shoe_description, specs, image_path FROM Shoes";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
-  // output data of each row
-  while($row = mysqli_fetch_assoc($result)) {
-    echo "id: " . $row["pid"]. " - Name: " . $row["shoe_name"]. " - Description " . $row["shoe_description"]. " - Specs:" .$row["specs"]. "<br>"."<br>";
-
-
-  }
+    // output data of each row
+    $products = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["pid"] . " - Name: " . $row["shoe_name"] . " - Description " . $row["shoe_description"] . " - Specs:" . $row["specs"] . " - Image:" . $row['image_path'] . "<br>" . "<br>";
+        array_push($products, $row);
+    }
 } else {
-  echo "0 results";
+    echo "0 results";
 }
-session_destroy();
-preShow($_SESSION);
+// session_destroy();
+$_SESSION['products'] = $products;
+// preShow($_SESSION['products']);
 
 mysqli_close($conn);
 ?>
