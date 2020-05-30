1d0cc7429dcc891aab801363d3379bc373ac27b8
ManPhamQuang <s3804811@rmit.edu.vn>
2020-05-30 17:07:51 +0700

    added cart layout

--- a5/cart.php
+++ a5/cart.php
@@ -1,3 +1,10 @@
+<?php
+require_once('config.php');
+require_once('functions.php');
+include 'navbar.php';
+include 'tools.php';
+$total = 0;
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,19 +13,89 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" type="text/css" href="css/index.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/cart.css">
 </head>
 
 <body>
-    <?php
-    require_once('config.php');
-    require_once('functions.php');
-    include 'navbar.php';
-    include 'tools.php';
-    ?>
-    <?php preShow($_SESSION) ?>
-
+    <div class="container p-2">
+        <div class="cart-header">
+            <h1 class="cart-header-content">Your cart | <?php echo count($_SESSION['cart']); ?> ITEM(s)</h1>
+            <p class="mb-1 cart-header-content text-muted"><a href="index.php">CONTINUE SHOPPING</a></p>
+        </div>
+        <div class="row">
+            <div class="col-lg-8 col-md-12">
+                <div class="container ">
+                    <?php
+                    foreach ($_SESSION['cart'] as $index => $productArray) {
+                        // $image = $productArray['image_path'];
+                        foreach ($productArray as $key => $value) {
+                            $$key = $value;
+                        }
+                        $formated_price = "$ " . number_format($price * $amount, 2);
+                        $total += $price * $amount;
+                        echo
+                            "<div class='row p-1 product-list'>
+                                <div class='col-lg-3 col-md-12'>
+                                    <img class='product-image' src=$image_path>
+                                </div>
+                                <div class='col-lg-9 col-md-12'>
+                                    <p>Product name: <b>$shoe_name</b></p>
+                                    <p>size: <b>$size</b></p>
+                                    <p>Amount: <b>$amount</b></p>
+                                    <p>Price: $amount*$price = <b>$formated_price</b></p>
+                                </div>
+                            </div>";
+                    }
+                    ?>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-12">
+                <div class="jumbotron p-2">
+                    <h2>ORDER SUMMARY</h2>
+                    <div class="container summary-box">
+                        <div class="non-breaker"></div>
+                        <p><?php echo count($_SESSION['cart']); ?> PRODUCTS</p>
+                        <div class="breaker"></div>
+                        <div class="cart-header">
+                            <p>Products Total:</p>
+                            <p><b><?php echo "$ " . number_format($total, 2); ?></b></p>
+                        </div>
+                        <div class="breaker"></div>
+                        <div class="cart-header">
+                            <p>Delivery:</p>
+                            <p><b>Free</b></p>
+                        </div>
+                        <div class="breaker"></div>
+                        <div class="cart-header">
+                            <p><b>ORDER TOTAL:</b></p>
+                            <p><b><?php echo "$ " . number_format($total, 2); ?></b></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="jumbotron p-2">
+                    <div class="container summary-box p-2">
+                        <form>
+                            <div class="form-group">
+                                <label for="cust-name">Name</label>
+                                <input type="text" class="form-control" name="cust[name]" id="cust-name" aria-describedby="name field" placeholder="Enter name">
+                                <small id="name-help" class="form-text text-muted">We'll never share your credentials with anyone else.</small>
+                            </div>
+                            <div class="form-group">
+                                <label for="cust-address">Address</label>
+                                <input type="password" class="form-control" name="cust[address]" id="cust-address" placeholder="address">
+                            </div>
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Confirm purchase</label>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
