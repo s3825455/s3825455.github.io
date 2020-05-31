6ada1c0b88c62fbbd939bf81899b24a2d7865256
ManPhamQuang <s3804811@rmit.edu.vn>
2020-05-31 15:46:27 +0700

    removed error reporting

--- a5/single_product.php
+++ a5/single_product.php
@@ -2,9 +2,9 @@
 <?php include('functions.php'); ?>
 <?php include 'tools.php' ?>
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 if (isset($_GET['pid'])) {
     $product = getProduct($_GET['pid']);
 }
